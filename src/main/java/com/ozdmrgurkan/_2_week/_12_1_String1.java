621e1c89f415e7126c9ccfecab20f542b716e461
Gürkan Özdemir <ozdmr.grkan3434@gmail.com>
2025-05-26 17:14:00 +0300

    String

--- src/main/java/com/ozdmrgurkan/_2_week/_12_1_String1.java
+++ src/main/java/com/ozdmrgurkan/_2_week/_12_1_String1.java
@@ -18,8 +18,9 @@ public class _12_1_String1 {
         System.out.println("Harf sayısı: " + vocabulary.length());
 
         //equals()
-        System.out.println(vocabulary==vocabulary2);
+        System.out.println(vocabulary == vocabulary2);
         System.out.println(vocabulary.equals(vocabulary2));
+        System.out.println(vocabulary.equalsIgnoreCase(vocabulary2));
 
         //charAt : Sıfırdan saymaya başlar ve verdiğimiz sayı ilgili
         System.out.println(vocabulary.charAt((4)));
@@ -34,14 +35,29 @@ public class _12_1_String1 {
         System.out.println("küçük harf: " + vocabulary.toLowerCase());
 
         //startsWith()
+        //endsWtih()
         System.out.println(vocabulary.startsWith("") + " :  ile mi başlıyor?");
         System.out.println(vocabulary.endsWith("m") + " : m ile mi bitiyor?");
 
 
+        //substring: parçalama --- Sıfırdan başlıyor.
+        // 2 yöntemle parçalanır.
+        // 1- süpürme --- 2-aralık alarak
+        System.out.println(vocabulary.substring(1));
+        System.out.println(vocabulary.trim().substring(1));
+        System.out.println(vocabulary.substring(1).toLowerCase());
+        System.out.println(vocabulary.substring(1, 10)); // 1 <= vocabulary < 10
+
+        // String vocabulary3 = "Merhaba"; // length = 7/
+        // System.out.println(vocabulary3.substring(1, 10)); // Hata verir. (StringIndexOutOfBoundsExeption)
 
 
-
-
+        String vocabulary3 = "Merhaba"; // length = 7
+        if (vocabulary3.length() >= 10) {
+            System.out.println(vocabulary3.substring(1, 10));
+        } else {
+            System.out.println("Verdiğiniz kelimenin en fazla " + vocabulary.length() + " karakteri vardır.");
+        }
 
 
     }
