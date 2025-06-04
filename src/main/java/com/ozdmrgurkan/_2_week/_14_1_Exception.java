60153308c52d9e32859af5d29c0305f508a34a52
Gürkan Özdemir <ozdmr.grkan3434@gmail.com>
2025-06-04 15:26:14 +0300

    try, catch, finally, throws, throw

--- src/main/java/com/ozdmrgurkan/_2_week/_14_1_Exception.java
+++ src/main/java/com/ozdmrgurkan/_2_week/_14_1_Exception.java
@@ -2,35 +2,45 @@ package com.ozdmrgurkan._2_week;
 
 import java.io.IOException;
 import java.util.Scanner;
-
+/*
+       Exception in thread "main" java.lang.ArithmeticException: / by zero
+       at com.ozdmrgurkan._2_week._14_1_Exception.main(_14_1_Exception.java:7)
+        */
 public class _14_1_Exception {
-    public static void main(String[] args) throws ArithmeticException, IOException,NullPointerException {
-        Scanner scanner = new Scanner(System.in);
-        int number1, number2;
-        System.out.println("Number 1 giriniz:");
-        number1 = scanner.nextInt();
-        System.out.println("Number 2 giriniz:");
-        number2 = scanner.nextInt();
 
-        try {
-            int result = number1 / number2;
-            System.out.println(result);
+    public static void calcula()throws ArithmeticException, IOException,NullPointerException
+        {
+            Scanner scanner = new Scanner(System.in);
+            int number1, number2;
+            System.out.println("Number 1 giriniz:");
+            number1 = scanner.nextInt();
+            System.out.println("Number 2 giriniz:");
+            number2 = scanner.nextInt();
 
-        } catch (ArithmeticException ai) {
-            ai.printStackTrace();
-            // ai.getMessage();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("İstisna olsa da olmasa da mutlaka yapılacak iş");
-            scanner.close();
-        }
+            try {
+                int result = number1 / number2;
+                System.out.println(result);
+
+            } catch (ArithmeticException ai) {
+                ai.printStackTrace();
+                // ai.getMessage();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("İstisna olsa da olmasa da mutlaka yapılacak iş");
+                scanner.close();
+            }
+
+            System.out.println("Son 10.000 satır");
+
+    }
+    public static void main(String[] args) throws IOException {
+        calcula();
+
+        //Bilerek istisna gönder
+        throw new NullPointerException("Neden boş verdiniz");
 
 
-        /*
-        Exception in thread "main" java.lang.ArithmeticException: / by zero
-	    at com.ozdmrgurkan._2_week._14_1_Exception.main(_14_1_Exception.java:7)
-         */
-        System.out.println("Son 10.000 satır");
+
     }
 }
