1bb86f8afab129afa7c24708ed1d3797e5778cdd
Gürkan Özdemir <ozdmr.grkan3434@gmail.com>
2025-06-30 21:49:05 +0300

    Formatter

---
+++ src/main/java/com/ozdmrgurkan/_2_week/_12_2_StringFormat.java
@@ -0,0 +1,16 @@
+package com.ozdmrgurkan._2_week;
+import java.util.Formatter;
+
+public class _12_2_StringFormat {
+
+
+    public static void formatter1(){
+        Formatter formatter= new Formatter();
+        formatter.format(" Merhabalar,Adınız : %s , Numaranız : %d, Bütçe: %f ","Gürkan",152,44.2);
+        System.out.println(formatter);
+    }
+
+    public static void main(String[] args) {
+formatter1();
+    }
+}
