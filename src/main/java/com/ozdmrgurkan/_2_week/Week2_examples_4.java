5575788d4434a23e2c768a4b2c5fec64f682bf4a
Gürkan Özdemir <ozdmr.grkan3434@gmail.com>
2025-07-25 00:57:03 +0300

    faktoriyel

--- src/main/java/com/ozdmrgurkan/_2_week/Week2_examples_4.java
+++ src/main/java/com/ozdmrgurkan/_2_week/Week2_examples_4.java
@@ -11,20 +11,31 @@ public class Week2_examples_4 {
 
         //Scanner
         Scanner veriGirisi = new Scanner(System.in);
+while (true) {
+    System.out.println(_15_4_SpecialColor.BLUE + " Faktöriyelini bulmak istediginiz sayıyı giriniz:");
+    sayi = veriGirisi.nextInt();
 
-        System.out.println(_15_4_SpecialColor.BLUE + " Faktöriyelini bulmak istediginiz sayıyı giriniz:");
-        sayi = veriGirisi.nextInt();
+    if (sayi < 0) {
+        System.out.println(_15_4_SpecialColor.BLUE + " Negatif değer girdiniz!" + _15_4_SpecialColor.RESET);
+       // sayi = Math.abs(sayi);
+        break; // 1.YOL (Yalnızca bulundugu döngü veya switch blogunu etkiliyor.)
+        // System.exit();  2.YOL ( JVM yi kapatarak programı tamamen sonlandırıyor.
+    } else if (sayi == 0) {
+        System.out.println(_15_4_SpecialColor.BLUE + sayi + _15_4_SpecialColor.BLUE + " sayısının faktöriyeli = 1 ' dir.");
+        fakt=1;
 
-        if (sayi < 0) {
-            System.out.println(_15_4_SpecialColor.BLUE + " Negatif değer girdiniz!" + _15_4_SpecialColor.RESET);
-            sayi = Math.abs(sayi);
-        } else if (sayi == 0) {
-            System.out.println(_15_4_SpecialColor.BLUE + sayi + _15_4_SpecialColor.BLUE + " sayısının faktöriyeli = 1 ' dir.");
-        } else {
-            for (int i = 1; i <= sayi; i++) {
-                fakt *= i;
-            }
-            System.out.println(_15_4_SpecialColor.BLUE + sayi + _15_4_SpecialColor.RED + " sayısının faktöriyeli = " + fakt);
+    }
+    else {
+        fakt=1;
+        for (int i = 1; i <= sayi; i++) {
+            fakt *= i;
+
         }
+        System.out.println(_15_4_SpecialColor.BLUE + sayi + _15_4_SpecialColor.RED + " sayısının faktöriyeli = " + fakt);
+
+    }
+}
+
+
     }
 }
